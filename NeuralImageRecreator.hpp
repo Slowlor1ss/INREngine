28df24633f5258db1e48c117d626499b1c76213c
Slowlor1ss <85391319+Slowlor1ss@users.noreply.github.com>
2026-08-13 17:18:14 +0100

    Fixed %ull not working on MSVC

--- NeuralImageRecreator.hpp
+++ NeuralImageRecreator.hpp
@@ -99,7 +99,7 @@ static engineFloat RunGPUTrainingEpoch(
         int inOffset = currentImageIdx * inputChannels;
         int tarOffset = currentImageIdx * targetChannels;
 
-    	PROFILE_PUSH("GPU Batch %llu", currentEpoch);
+    	PROFILE_PUSH("GPU Batch %d", currentEpoch);
         // Execute purely on the GPU (No PCIe transfer!)
         gpuNet.TrainBatchGPU(
             gpuData.d_inputAct + inOffset,
